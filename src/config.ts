ec8278b87520327fedf20a009ba4d4a9b3d1c484
Linhle1999 <linhlee20101999@gmail.com>
2023-08-22 15:25:41 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'linhlnq.us.auth0.com', // Auth0 domain
   clientId: 'mdPWuap3k6ara33KHBpsTS04rxUWpHgk', // Auth0 client id
-  callbackUrl: 'http://project-5-fe.s3-website-us-east-1.amazonaws.com/callback'
+  callbackUrl: 'http://https://d17kstkl8r5t9a.cloudfront.net/callback'
 }
