113fde606dd658b2a8699ab1c04db2603f5a7ded
Linhle1999 <linhlee20101999@gmail.com>
2023-08-22 16:53:28 +0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'linhlnq.us.auth0.com', // Auth0 domain
   clientId: 'mdPWuap3k6ara33KHBpsTS04rxUWpHgk', // Auth0 client id
-  callbackUrl: 'http://https://d17kstkl8r5t9a.cloudfront.net/callback'
+  callbackUrl: 'https://d17kstkl8r5t9a.cloudfront.net/callback'
 }
